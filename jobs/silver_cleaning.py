75111188fd63f1d8902f656294a81593d454c292
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-12 23:14:23 -0500

    Create silver_cleaning module

---
+++ jobs/silver_cleaning.py
@@ -0,0 +1,13 @@
+# jobs/silver_cleaning.py
+
+from pyspark.sql.functions import col, to_date
+
+
+def clean_transaction_data(df, silver_path):
+    """
+    Silver Layer Job:
+    - Removes duplicates
+    - Drops null values
+    - Converts txn_date to DateType
+    - Renames amount column
+    """
