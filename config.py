67681913d1797a5a6828942238377628bf5eab75
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-12 22:57:19 -0500

    Create config.py paths

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+RAW_FILE = "data/transactions.csv"
+
+BRONZE_PATH = "output/bronze/"
+SILVER_PATH = "output/silver/"
+FRAUD_PATH = "output/silver/fraud_flagged/"
+GOLD_PATH = "output/gold/"
+REPORT_PATH = "output/reports/"
