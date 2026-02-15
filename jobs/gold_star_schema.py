c2c6fd59b49c38003172e9d0048c2aa5511a7f68
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-14 23:50:49 -0500

    Implement build_star_schema() methods

---
+++ jobs/gold_star_schema.py
@@ -0,0 +1,11 @@
+# jobs/gold_star_schema.py
+
+def build_star_schema(df, gold_path):
+    """
+    Gold Layer Job:
+    Builds Star Schema tables:
+    - dim_customer
+    - dim_merchant
+    - fact_transactions
+    """
+    return fact_transactions
