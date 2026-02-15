fda03a6c145df4d2b9738821e6a6e64bcbc43e43
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-15 00:08:32 -0500

    Implement main() method

---
+++ main.py
@@ -0,0 +1,23 @@
+# main.py
+
+from config import RAW_FILE, BRONZE_PATH, SILVER_PATH, FRAUD_PATH, GOLD_PATH
+from utils.spark_session import get_spark_session
+
+from jobs.bronze_ingestion import ingest_transactions
+from jobs.silver_cleaning import clean_transaction_data
+from jobs.fraud_detection import fraud_detection
+from jobs.gold_star_schema import build_star_schema
+
+from jobs.business_queries import (
+    top_spending_customers,
+    fraud_transaction_percentage,
+    revenue_by_merchant,
+    spending_by_city
+)
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
